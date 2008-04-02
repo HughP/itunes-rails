8725ad7e948f9022cd9a13e048ecd7b18fdc0955
dhchoi <dhchoi@fd67bc98-d149-0410-94e1-4941a2748d15>
2008-04-02 23:37:58 +0000

    fixed a pagination bug

--- trunk/app/controllers/playlists_controller.rb
+++ trunk/app/controllers/playlists_controller.rb
@@ -15,10 +15,10 @@ class PlaylistsController < ApplicationController
         @tracks = @playlist.tracks.sort_by {|t| t.send(order)}[0,500]
       else
         @offset = (params[:offset] || 0).to_i
-        limit = 500
-        @tracks = @playlist.tracks[@offset,limit]
+        @limit = 500
+        @tracks = @playlist.tracks[@offset,@limit]
         @total = @playlist.tracks.size 
-        @more = [@total, limit].min
+        @more = [@total - @offset, @limit].min
       end
     end
   end
