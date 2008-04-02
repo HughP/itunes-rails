a93b2cea1cdc98938d489fdee1d165f72fd9ffd4
dhchoi <dhchoi@fd67bc98-d149-0410-94e1-4941a2748d15>
2008-04-02 21:42:12 +0000

    removing extraneous files

--- trunk/db/migrate/006_remove_name_column_from_listings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNameColumnFromListings < ActiveRecord::Migration
-  def self.up
-    remove_column :listings, :name
-  end
-
-  def self.down
-    add_column :listings, :name, :string
-  end
-end
--- trunk/db/migrate/008_add_index_on_track.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnTrack < ActiveRecord::Migration
-  def self.up
-
-  end
-
-  def self.down
-
-  end
-end
