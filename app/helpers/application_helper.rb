cd81ad292079da4713abd5a87e5bb6f4603e734f
dhchoi <dhchoi@fd67bc98-d149-0410-94e1-4941a2748d15>
2008-04-02 23:50:46 +0000

    enabled paging and keeping the current sort order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # includes the current request parameters
+  def url_for_with_current_params(new_params={})
+    old_params = params
+    url_for(old_params.merge(new_params))
+  end
 end
