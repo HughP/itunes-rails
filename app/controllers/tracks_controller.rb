8a9b44ddaa2e0e18a8ed5a2bc48530ec199d6171
dhchoi <dhchoi@fd67bc98-d149-0410-94e1-4941a2748d15>
2008-04-08 16:59:05 +0000

    reordering now possible

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,28 +2,27 @@ class TracksController < ApplicationController
 
   before_filter :find_track
   def queue
+    start_playing_at_index = @queued_tracks.size  # might use this later
     # queuing multiple tracks
     if params[:tracks]
       track_ids = params[:tracks].map {|track_id| track_id.to_i}
+      tracks = track_ids.collect{ |id| @iTunes.find_track(id) }
       logger.debug "queuing multiple tracks"
-    else 
-      track_ids = [params[:id].to_i]
-    end
-
-    track_ids.each do |track_id|
+      @iTunes.queue_tracks(tracks, session[:username] || "")
+    elsif params[:id] && params[:id] != "multiple"  # queuing one track
+      track_id = params[:id].to_i
       track = @iTunes.find_track(track_id)
-      track.comment = session[:username] || "" # credit for queuing the song
+      logger.debug track
       track.enabled = true # just in case
+      track.comment = session[:username] || "" # credit for queuing the song
+      logger.debug "queuing a track"
       @iTunes.queue_track(track)
-      logger.debug "queuing"
-      logger.debug @state
     end
-
     if @state.strip.to_s == "stopped" 
       logger.debug "TRYING TO PLAY"
       @iTunes.queue.playOnce(1)
-      # but need to skip to the head of the queue
-      (@queued_tracks.size - 1).times do 
+      # but need to skip to the head of the queue, unless multiple track add
+      start_playing_at_index.times do 
         @iTunes.nextTrack
       end
     end
@@ -35,6 +34,22 @@ class TracksController < ApplicationController
           page.replace("queue-box", :partial => "queued_tracks/queued_tracks") 
         } 
       } 
+    end
+  end
+
+  def reorder
+    @iTunes.clear_queue
+    track_ids = params[:queued_tracks].map {|track_id| track_id.to_i}.reverse
+    tracks = track_ids.collect{ |id| @iTunes.find_track(id) }
+    logger.debug "reordering tracks"
+    @iTunes.queue_tracks(tracks)
+    respond_to do |format|
+      format.js { 
+        reload_state_data 
+        render( :update ) { | page | 
+          page.replace("queue-box", :partial => "queued_tracks/queued_tracks") 
+        } 
+      }
     end
   end
 
